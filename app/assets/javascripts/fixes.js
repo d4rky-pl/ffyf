f30450af6ecd90dcaeb6fc53cf3f0bea42979b1d
Michał Matyas <github@higher.lv>
2013-10-19 21:29:25 +0200

    Refactor checkbox code

--- app/assets/javascripts/fixes.js
+++ app/assets/javascripts/fixes.js
@@ -1,12 +1,16 @@
 $(function() {
+    var checkbox = function(t,p) {
+        if(t.prop('checked')) {
+            p.addClass('active');
+        } else {
+            p.removeClass('active');
+        }
+    }
     $('label > input[type=checkbox]').each(function() {
         var t = $(this), p = t.parent('label');
         t.change(function() {
-            if(t.prop('checked')) {
-                p.addClass('active');
-            } else {
-                p.removeClass('active');
-            }
+            checkbox(t,p);
         })
+        checkbox(t,p);
     });
 })
