000d03832bf76659d5d0274203c5d427da3a3826
katafrakt <inquebrantable@gmail.com>
2013-10-19 18:19:02 +0200

    move instagram data to config file

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,7 +1,6 @@
-#require 'instagram'
-CALLBACK_URL = "http://localhost:4567/oauth/callback"
+yaml = YAML.load(File.open("#{Rails.root}/config/instagram.yml"))
 
 Instagram.configure do |config|
-  config.client_id = "8ff397f65c3a4a08950a29b0c1dee1c8"
-  config.access_token = "576249041.8ff397f.976b9ea6273a400d888328a6b9a93b45"
+  config.client_id = yaml['instagram']['client_id']
+  config.access_token = yaml['instagram']['access_token']
 end
