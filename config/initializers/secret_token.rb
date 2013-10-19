8cbf1f827b15ea8a10f6e207670bc06611b0bb9a
Michał Matyas <github@higher.lv>
2013-10-19 10:52:49 +0200

    Add Rails4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jogger3::Application.config.secret_key_base = '7978105c5960c58aa39391637e348e598649a09fa1ef7a116404acd196d065ace02bbb23931938913f4a82a1da56171d99d77d541b5aada3132afcf5a01b3de7'
