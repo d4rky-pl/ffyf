f3c21ac2e8982f3a278f1205c0a65e2f34b680a6
Krzysztof Kucharski <krzysztof@kucharski.net.pl>
2013-10-20 20:27:54 +0200

    add address to place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,11 @@ class Place < ActiveRecord::Base
   validates :id_instagram, uniqueness: true
 
   acts_as_gmappable :lat => 'lat', :lng => 'long', :process_geocoding => false
+
+  after_save :fetch_address
+
+  def fetch_address
+    adr = Geocoder.search("#{self.lat},#{self.long}").first.formatted_address
+    record.address = adr
+  end
 end
--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PlaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "address fetching" do
+    p = Place.create(:name => "testing", :lat => 44.981667, :long => -93.27833, :id_instagram => 666)
+    p.save!
+    sleep(5)
+    puts p.address
+    assert p.address
+  end
 end
